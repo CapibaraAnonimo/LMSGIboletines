8b20c4ce6512fb21acf2d4ba8bd5e61b444a0794
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-03-22 10:48:19 +0100

    Hecho el ejercicio 5

---
+++ html/Tema8/ejercicio5/ejercicio5.js
@@ -0,0 +1,11 @@
+let n1 = 5;
+let n2 = 8;
+
+if (n1 < n2)
+    alert("El primer numero es mayor que el segundo número")
+if (n2 % 2 === 0)
+    alert("El segundo número es positivo")
+if(n1 < 0 || n1 > 0)
+    alert("El primer número es distinto de 0")
+if (n1++ >= n2)
+    alert("Incrementar en una unidad el primer número no lo hace mayor o igual que el segundo número")
