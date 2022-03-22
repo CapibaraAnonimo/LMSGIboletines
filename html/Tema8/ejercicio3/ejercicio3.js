993e91d247244f1598eaa767370839dcd4291d37
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-03-22 09:07:39 +0100

    Hecho el ejercicio 3

---
+++ html/Tema8/ejercicio3/ejercicio3.js
@@ -0,0 +1,4 @@
+let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubr    e", "Nombiembre", "Diciembre"];
+for (let mes of meses) {
+    alert(mes);
+}
