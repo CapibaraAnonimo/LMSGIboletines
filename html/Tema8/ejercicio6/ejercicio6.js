d41a4ca946d37d9ea38192770824e2dd914abc27
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-03-22 12:00:07 +0100

    Hecho el ejercicio 6

---
+++ html/Tema8/ejercicio6/ejercicio6.js
@@ -0,0 +1,16 @@
+let numeroDNI = 0;
+let letraDNI;
+let posicionLetra = 0;
+let letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+
+nuermoDNI = prompt("Introduzca el número del DNI", "0");
+letraDNI = prompt("Introduzca la letra del DNI", "T")
+if (~~numeroDNI > 0 || ~~numeroDNI < 99999999) {
+    posicionLetra = ~~numeroDNI%23;
+    if(letras[posicionLetra] === letraDNI)
+        alert("Su DNI es válido")
+    else
+        alert("Su DNI no es válido")
+} else
+    alert("Número no valido")
+
