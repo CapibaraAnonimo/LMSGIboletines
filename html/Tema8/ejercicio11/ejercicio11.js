200336ba53de5162f9cb3436544b39592fc549c2
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-03-23 14:23:57 +0100

    A medias de terminar el ejercicio 11

---
+++ html/Tema8/ejercicio11/ejercicio11.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    // Número de enlaces de la página
+    let enlaces = document.querySelectorAll("a").length;
+    alert(enlaces);
+
+    // Dirección del penúltimo enlace
+    let penultimoEnlace = document.querySelectorAll("a")[enlaces - 2];
+    alert(penultimoEnlace)
+
+    // Numero de enlaces que apuntan a http://prueba
+    let enlacesPrueba = document.querySelector("");
+
+    // Numero de enlaces del tercer p�rrafo
+
+};
