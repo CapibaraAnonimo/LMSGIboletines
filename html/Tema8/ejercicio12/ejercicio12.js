514b0372cec7b5047cffaf484e8942c32ff47871
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-03-25 10:10:47 +0100

    A medias con el ejercicio 12

--- html/Tema8/ejercicio12/ejercicio12.js
+++ html/Tema8/ejercicio12/ejercicio12.js
@@ -1,5 +1,10 @@
-
-
-function muestra(){
-
+function muestra() {
+    /*let a = document.getElementsByTagName("enlace");
+    let nuevoTexto = document.createElement("p");
+    nuevoTexto.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci consequatur corporis debitis dignissimos ducimus eius ipsa ipsam molestiae non qui ratione rem, rerum sunt voluptas! Autem cupiditate iure praesentium?";
+    a.append(nuevoTexto);*/
+    let antiguo = document.getElementsByTagName("enlace")[0];
+    let nuevo = document.createElement("p");
+    nuevo.innerHTML = " COSAS NUEVAS";
+    antiguo.parentNode.replaceChild(nuevo, antiguo);
 }
