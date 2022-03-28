4ce1bdbf6ff523af2a4001450c2f2dadebb145d7
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-03-28 14:26:26 +0200

    He avanzado un poco con el ejercicio 12

--- html/Tema8/ejercicio12/ejercicio12.js
+++ html/Tema8/ejercicio12/ejercicio12.js
@@ -1,11 +1,16 @@
+document.getElementById("texto").style.display = "none";
+
 function muestra() {
     /*let a = document.getElementsByTagName("enlace");
     let nuevoTexto = document.createElement("p");
     nuevoTexto.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci consequatur corporis debitis dignissimos ducimus eius ipsa ipsam molestiae non qui ratione rem, rerum sunt voluptas! Autem cupiditate iure praesentium?";
     a.append(nuevoTexto);*/
-    let antiguo = document.getElementsByTagName("enlace")[0];
+    /*let antiguo = document.getElementsByTagName("enlace")[0];
     let nuevo = document.createElement("p");
     nuevo.innerHTML = " COSAS NUEVAS";
     antiguo.innerHTML = "";
-    antiguo.appendChild(nuevo);
+    antiguo.appendChild(nuevo);*/
+    document.getElementById("enlace").style.display = "none";
+    document.getElementById("texto").style.display = "block";
+
 }
