2549035364404c16673af3c0dbe271d7b23b797e
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-04-01 10:11:55 +0200

    Terminado el ejercicio 12, está chulo el algoritmo de la burbuja

--- html/Tema8/ejercicio12/ejercicio12.js
+++ html/Tema8/ejercicio12/ejercicio12.js
@@ -2,13 +2,12 @@ let cadena = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 alert(ordenar(cadena));
 
 function ordenar(cadena) {
-    let aux;
-    for (let i = cadena.length - 1; i > 0; i--) {
-        if(cadena[i] < cadena[0]){
-            aux = cadena[i];
-            cadena.unshift(i, 1, 0, aux);
-            i = cadena.length;
+    for (let i = 0; i < cadena.length - 1; i++) {
+        for (let j = 0; j < cadena.length - i - 1; j++) {
+            if (cadena[j + 1] < cadena[j]) {
+                [cadena[j], cadena[j + 1]] = [cadena[j + 1], cadena[j]];
+            }
         }
-        return cadena
     }
+    return cadena;
 }
