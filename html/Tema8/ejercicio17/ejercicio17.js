eb9b4aed04c54984fe78603cb11c2c876eb1eb8a
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-04-04 08:59:13 +0200

    Es muy raro el 17, no entiendo los eventos

---
+++ html/Tema8/ejercicio17/ejercicio17.js
@@ -0,0 +1,9 @@
+let cX;
+
+function muestraInformacion(mensaje) {
+    let info = document.getElementById("info");
+    info.innerHTML = '<h1>' + mensaje[0] + '</h1>';
+    for (let i = 1; i < mensaje.length; i++) {
+        info.innerHTML += '<p>' + mensaje[i] + '</p>';
+    }
+}
