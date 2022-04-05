1aff3b6be2cc493c9cfa0bf4e79c42c2739dc59f
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-04-05 13:42:54 +0200

    Casi he terminado el ejercicio 17, está bastante interesante al final

--- html/Tema8/ejercicio17/ejercicio17.js
+++ html/Tema8/ejercicio17/ejercicio17.js
@@ -2,8 +2,19 @@ document.body.addEventListener("mousemove", muestraInformacion)
 
 function muestraInformacion(mensaje) {
     let info = document.getElementById("info");
-    info.innerHTML = '<h1>' + mensaje[0] + '</h1>';
+    info.innerHTML = '<h1>' + "Ratón" + '</h1>';
+
+
     for (let i = 1; i < mensaje.length; i++) {
         info.innerHTML += '<p>' + mensaje[i] + '</p>';
     }
+
+
+    info.innerHTML += '<p>' + "Navegador [" + mensaje.clientX + ", " + mensaje.clientY + "]" + '</p>';
+    info.innerHTML += '<p>' + "Pagina [" + mensaje.pageX + ", " + mensaje.pageY + "]" + '</p>';
+    info.style.cssText = "border: black 2px solid;" +
+        "width: fit-content;" +
+        "padding: 10px;" +
+        "position: fixed;" +
+        "background-color: white;";
 }
