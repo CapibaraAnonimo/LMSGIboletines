60460594c95390ca0a8648b70d7f1a392ddfe7b7
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-04-18 09:13:29 +0200

    Terminado el ejercicio 17, bastante bien

--- html/Tema8/ejercicio17/ejercicio17.js
+++ html/Tema8/ejercicio17/ejercicio17.js
@@ -1,20 +1,40 @@
-document.body.addEventListener("mousemove", muestraInformacion)
+document.body.addEventListener("mousemove", muestraInformacion);
+document.body.addEventListener("mousedown", muestraInformacion);
+document.body.addEventListener("keypress", muestraInformacion);
 
 function muestraInformacion(mensaje) {
     let info = document.getElementById("info");
-    info.innerHTML = '<h1>' + "Ratón" + '</h1>';
+    if (mensaje.type === "mousemove" || mensaje.type === "mousedown") {
+        info.innerHTML = '<h1>' + mensaje.type + '</h1>';
 
 
-    for (let i = 1; i < mensaje.length; i++) {
-        info.innerHTML += '<p>' + mensaje[i] + '</p>';
+        for (let i = 1; i < mensaje.length; i++) {
+            info.innerHTML += '<p>' + mensaje[i] + '</p>';
+        }
+
+
+        info.innerHTML += '<p>' + "Navegador [" + mensaje.clientX + ", " + mensaje.clientY + "]" + '</p>';
+        info.innerHTML += '<p>' + "Pagina [" + mensaje.pageX + ", " + mensaje.pageY + "]" + '</p>';
+        info.style.cssText = "border: black 2px solid;" +
+            "width: fit-content;" +
+            "padding: 10px;" +
+            "position: fixed;" +
+            "background-color: white;";
+        if (mensaje.type === "mousedown") {
+            info.style.cssText = "border: black 2px solid;" +
+                "width: fit-content;" +
+                "padding: 10px;" +
+                "position: fixed;" +
+                "background-color: #FFFFCC;";
+        }
+    } else if (mensaje.type === "keypress") {
+        info.innerHTML = '<h1>' + mensaje.type + '</h1>';
+        info.innerHTML += '<p>' + "Carácter [" + mensaje.key + "]" + '</p>';
+        info.innerHTML += '<p>' + "Codigo [" + mensaje.keyCode + "]" + '</p>';
+        info.style.cssText = "border: black 2px solid;" +
+            "width: fit-content;" +
+            "padding: 10px;" +
+            "position: fixed;" +
+            "background-color: #CCE6FF;";
     }
-
-
-    info.innerHTML += '<p>' + "Navegador [" + mensaje.clientX + ", " + mensaje.clientY + "]" + '</p>';
-    info.innerHTML += '<p>' + "Pagina [" + mensaje.pageX + ", " + mensaje.pageY + "]" + '</p>';
-    info.style.cssText = "border: black 2px solid;" +
-        "width: fit-content;" +
-        "padding: 10px;" +
-        "position: fixed;" +
-        "background-color: white;";
 }
